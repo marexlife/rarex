49c5a1406035b157d71cd181e368daf2dec6e02b
marcel <marcel.hajek7@gmail.com>
2026-08-28 18:02:38 +0200

    better TokenStream

--- src/types/token_stream.rs
+++ src/types/token_stream.rs
@@ -6,23 +6,23 @@ use crate::types::token::{
 #[must_use]
 pub(crate) struct TokenStream {
     token_kinds: Vec<TokenKind>,
-    token_pos: Vec<TokenPos>,
+    token_poses: Vec<TokenPos>,
     progress: usize,
 }
 
 impl TokenStream {
     pub(crate) fn new(
         token_kinds: Vec<TokenKind>,
-        token_pos: Vec<TokenPos>,
+        token_poses: Vec<TokenPos>,
     ) -> Self {
         Self {
             token_kinds,
-            token_pos,
+            token_poses,
             progress: 0,
         }
     }
-    pub(crate) fn toks(&self) -> &Vec<Token> {
-        &self.token_kinds
+    pub(crate) fn poses(&self) -> TokenPos {
+        self.token_poses[self.progress]
     }
 
     pub(crate) fn kinds(&self) -> &TokenKind {
