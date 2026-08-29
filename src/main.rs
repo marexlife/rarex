87be9594b272cf07664a5bfd7ffccfe3ba124c5c
marcel <marcel.hajek7@gmail.com>
2026-08-29 07:46:33 +0200

    made very basic main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-use crate::actions::lex;
+use std::{error::Error, fs::File, io::Read};
+
+use crate::{
+    actions::lex, types::source_code::SourceCode,
+};
 
 mod actions;
 mod tests;
 mod types;
 
-fn main() {}
+fn main() -> Result<(), Box<dyn Error>> {
+    let args =
+        std::env::args().collect::<Vec<String>>();
+
+    for arg in args {
+        let mut buf: [u8; 10] = [0; 10];
+        let mut file = File::open(arg)?;
+        match file.read(&mut buf) {
+            Ok(a) => println!("{a}"),
+            Err(e) => return Err(Box::new(e)),
+        };
+
+        let _ = lex::lex(SourceCode::new(
+            String::from_utf8(buf.to_vec())?,
+        ));
+
+    }
+
+    Ok(())
+}
