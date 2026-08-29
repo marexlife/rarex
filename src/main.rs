e5c0a808ce3db52a8944ba8d2174cbd791238ee7
marcel <marcel.hajek7@gmail.com>
2026-08-29 07:52:23 +0200

    improved main

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         std::env::args().collect::<Vec<String>>();
 
     for arg in args {
-        let mut buf = String::new();
+        let mut buf: Vec<u8> = vec![];
         let mut file = File::open(arg)?;
 
-        match file.read_to_string(&mut buf) {
-            Ok(a) => println!("{a}"),
+        match file.read(&mut buf) {
+            Ok(a) => eprintln!("{a} characters read"),
             Err(e) => return Err(Box::new(e)),
         };
 
-        let _ = lex::lex(SourceCode::new(buf));
+        let code = String::from_utf8(buf)?;
+        let _ = lex::lex(SourceCode::new(code));
     }
 
     Ok(())
